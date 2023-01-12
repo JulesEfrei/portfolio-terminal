e07872e555782a4204d842dceeda1b0e616e2ab1
JulesEfrei <jules.bruzeau@efrei.net>
2023-01-12 16:14:52 +0100

    Fix : Type issue

--- src/components/atoms/Skills.tsx
+++ src/components/atoms/Skills.tsx
@@ -1,16 +1,26 @@
 import data from "../../utils/data.json";
 import "./styles/Skills.scss";
 
+type skills = {
+  "Common Stack": string[];
+  "Front-End": string[];
+  Mobile: string[];
+  "Back-end": string[];
+  Database: string[];
+  Programation: string[];
+  "What I'm learning": string[];
+};
+
 function Skills() {
   return (
     <>
       <div className="skills-component">
-        {Object.keys(data.skills).map((elm: String, index: Number) => {
+        {Object.keys(data.skills).map((elm, index: Number) => {
           return (
             <div className="container" key={`${elm}-${index}`}>
               <h4>{elm}</h4>
               <div className="item-container">
-                {data.skills[elm].map((item, i) => {
+                {data.skills[elm as keyof skills].map((item, i) => {
                   return <p key={`${item}-${i}`}>{item}</p>;
                 })}
               </div>
